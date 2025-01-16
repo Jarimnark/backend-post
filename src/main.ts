413b5b09b4dc84712b8abbf5d41282a21d8c49d0
Jarimnark <jarimnark@gmail.com>
2025-01-17 03:00:38 +0700

    10 attemp

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,43 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
+
+// let cachedServer: Server;
+
+// async function bootstrap(): Promise<Server> {
+//   if (!cachedServer) {
+//     const app = await NestFactory.create(AppModule);
+//     app.enableCors({
+//       origin: true,
+//       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//       credentials: true,
+//     });
+//     await app.listen(process.env.PORT ?? 3000);
+
+//     const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+//       app.getHttpAdapter().getInstance()(req, res);
+//     });
+
+//     cachedServer = server;
+//   }
+
+//   return cachedServer;
+// }
+
+// export default async function handler(
+//   req: IncomingMessage,
+//   res: ServerResponse,
+// ) {
+//   const server = await bootstrap();
+//   server.emit('request', req, res);
+// }
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
 
-let cachedServer: Server;
-
-async function bootstrap(): Promise<Server> {
-  if (!cachedServer) {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    });
-    await app.init();
-
-    const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-      app.getHttpAdapter().getInstance()(req, res);
-    });
-
-    cachedServer = server;
-  }
-
-  return cachedServer;
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-export default async function handler(
-  req: IncomingMessage,
-  res: ServerResponse,
-) {
-  const server = await bootstrap();
-  server.emit('request', req, res);
-}
+bootstrap();
