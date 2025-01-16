d0862491c438df872de0fa9099957878959adcba
Jarimnark <jarimnark@gmail.com>
2025-01-17 03:17:34 +0700

    13 a

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
