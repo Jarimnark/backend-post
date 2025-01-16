05e15c74d9dde5b41ff7f1b8d7c1e7161c35e853
Jarimnark <jarimnark@gmail.com>
2025-01-17 02:55:10 +0700

    9th attemp

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,30 @@ import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
 
 let cachedServer: Server;
 
-async function bootstrap() {
-  // if (!cachedServer) {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap(): Promise<Server> {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+    await app.init();
 
-  //   const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  //     app.getHttpAdapter().getInstance()(req, res);
-  //   });
+    const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+      app.getHttpAdapter().getInstance()(req, res);
+    });
 
-  //   cachedServer = server;
-  // }
+    cachedServer = server;
+  }
 
-  // return cachedServer;
+  return cachedServer;
 }
 
-// export default async function handler(
-//   req: IncomingMessage,
-//   res: ServerResponse,
-// ) {
-//   const server = await bootstrap();
-//   server.emit('request', req, res);
-// }
-
-bootstrap();
+export default async function handler(
+  req: IncomingMessage,
+  res: ServerResponse,
+) {
+  const server = await bootstrap();
+  server.emit('request', req, res);
+}
